47c271b20857eced442e608a82e1fbf1ad1f1d31
user <sammedtwumasi@gmail.com>
2025-01-08 12:43:40 +0000

    intial

---
+++ lib/feature/dashboard/presentation/dashboard.dart
@@ -0,0 +1,41 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:get/get.dart';
+import 'package:storekepper_desktop/feature/dashboard/presentation/pages.dart';
+
+import '../controller/dashboardcontroller.dart';
+
+class DashboardScreen extends StatelessWidget {
+  DashboardController controller = Get.put(DashboardController());
+
+
+  PaneDisplayMode displayMode = PaneDisplayMode.open;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return NavigationView(
+        appBar: const NavigationAppBar(
+          automaticallyImplyLeading: false,
+          title: Text('My Store App'),
+        ),
+        pane: NavigationPane(
+          selected: controller.selectedPage.value,
+          onItemPressed: (index) {
+            print(index);
+            controller.selectedPage.value = index;
+          },
+          onChanged: (index) {},
+          items: items,
+          footerItems: [
+            PaneItem(
+              icon: const Icon(FluentIcons.settings),
+              title: const Text('Settings'),
+              body: Container(),
+            ),
+          ],
+        ),
+      );
+    });
+    ;
+  }
+}
